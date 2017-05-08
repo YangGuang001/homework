c462703f1c4e1eae2ddf610ba8abe669f42f622b
yangGuang001 <824901900@qq.com>
2017-05-08 22:55:56 +0800

    first

--- mywork_first/src/main/java/Node.java
+++ mywork_first/src/main/java/Node.java
@@ -1,47 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Created by yz on 2017/5/7.
  */
+@Data
+@AllArgsConstructor
 public class Node {
     private char data;
     private Node lChild;
     private Node rChild;
-
-    public Node(char data,Node lChild,Node rChild){
-        this.data = data;
-        this.lChild = lChild;
-        this.rChild = rChild;
-    }
-
-    public char getData() {
-        return data;
-    }
-
-    public void setData(char data) {
-        this.data = data;
-    }
-
-    public Node getlChild() {
-        return lChild;
-    }
-
-    public void setlChild(Node lChild) {
-        this.lChild = lChild;
-    }
-
-    public Node getrChild() {
-        return rChild;
-    }
-
-    public void setrChild(Node rChild) {
-        this.rChild = rChild;
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "data=" + data +
-                ", lChild=" + lChild +
-                ", rChild=" + rChild +
-                '}';
-    }
 }
